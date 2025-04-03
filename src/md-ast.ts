f2fe49359baeba7d5c717f304cdf707b74ed0e19
Yann Leflour <yann.leflour.ext@qonto.com>
2025-04-03 11:57:02 +0200

    xml test

--- src/md-ast.ts
+++ src/md-ast.ts
@@ -1,8 +1,50 @@
+import { t } from "@synstack/text";
 import remarkParse from "remark-parse";
 import { unified } from "unified";
 
 const processor = unified().use(remarkParse);
 
-const ast = processor.parse("# Hello World");
+const unendedCodeBlock = t`
+  \`\`\`typescript
+  const a = 1;
+`;
 
-console.dir(ast, { depth: null });
+// Works
+console.dir(processor.parse(unendedCodeBlock), { depth: null });
+
+const xmlTag = t`
+  # xmlTag
+  <div>xmlTag</div>
+`;
+
+// Outputs as "html" type
+console.dir(processor.parse(xmlTag), { depth: null });
+
+const unendedXmlTag = t`
+  # unendedXmlTag
+  <div>unendedXmlTag
+`;
+
+// Outputs as "html" type
+console.dir(processor.parse(unendedXmlTag), { depth: null });
+
+const unclosedXmlTag = t`
+  # partialAttribute
+  <div class="partialAttribute"
+`;
+
+// Outputs as "html" type
+console.dir(processor.parse(unclosedXmlTag), { depth: null });
+
+const unfinishedXmlTag = t`
+  # unfinishedXmlTag
+  <di
+`;
+
+// Outputs as "paragraph" type
+console.dir(processor.parse(unfinishedXmlTag), { depth: null });
+
+const xmlBracket = "<";
+
+// Outputs as "paragraph" type
+console.dir(processor.parse(xmlBracket), { depth: null });
